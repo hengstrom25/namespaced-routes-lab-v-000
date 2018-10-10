12eaadd76d2ce76759e7124ca8fc27f986ca390e
Heidi Engstrom <hengstrom25@gmail.com>
2018-10-10 16:44:13 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -10,7 +10,8 @@ class ArtistsController < ApplicationController
   def new
     @preference = Preference.new
     @artist = Artist.new
-    if !preference.allow
+    if !preference.allow_create_artists
+      redirect_to artists_path
   end
 
   def create
