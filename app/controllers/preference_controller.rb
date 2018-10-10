1097c68293bf67007e3c50d2fe23094a63508121
Heidi Engstrom <hengstrom25@gmail.com>
2018-10-10 15:11:08 +0000

    automatically backed up by learn

--- app/controllers/preference_controller.rb
+++ app/controllers/preference_controller.rb
@@ -1,8 +1,8 @@
-class Admin::StatsController < ApplicationController
+class Admin::PreferenceController < ApplicationController
 
-  def index
-    @song_count = Song.count
-    @artists_count = Artist.count
+  #def index
+    #@song_count = Song.count
+    #@artists_count = Artist.count
     #@last_post = Post.last
-  end
+  #end
 end
